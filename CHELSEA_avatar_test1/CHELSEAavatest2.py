78b33b226c0b2225db3fa8d04c382e6825cbfa65
stringzzz <119706411+stringzzz@users.noreply.github.com>
2024-03-17 21:51:24 -0700

    Update CHELSEAavatest2.py

--- CHELSEA_avatar_test1/CHELSEAavatest2.py
+++ CHELSEA_avatar_test1/CHELSEAavatest2.py
@@ -1,3 +1,20 @@
+#   chatbotCHELSEA, an AI chatbot with simulated emotions, math logic, and some self identity
+#   This version includes an animated avatar
+#   Copyright (C) 2024 stringzzz, Ghostwarez Co.
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import pygame
 import threading
 import queue
