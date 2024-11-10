eabfd98755ac20058f0f4d0fe6f237c6beb51387
stringzzz <119706411+stringzzz@users.noreply.github.com>
2024-11-09 16:04:56 -0800

    Now works with both bigrams and trigrams

---
+++ CHELSEA_BETA13/beta12to13conversion.py
@@ -0,0 +1,58 @@
+#If you have been using the previous 'BETA12' version, use this script with your memory files to produce
+# The new 'bigramDictionary.json' memory file for the 'BETA13' version
+#Now includes code for building trigram dictionary!
+
+import json
+import re
+
+message_dict2 = {}
+with open("messageDictionary2.json", 'r') as message_dictionary_file:
+    message_dict2 = json.load(message_dictionary_file)
+
+dictionary = {}
+with open("dictionary.json", 'r') as dictionary_file:
+	dictionary = json.load(dictionary_file)
+
+nEmotions = ["happy", "angry", "sad", "afraid"]
+
+bigram_dictionary = {}
+
+for emotion in nEmotions:
+    messages = [message for message in message_dict2[emotion].keys()]
+    for response in message_dict2[emotion].values():
+        messages.extend(response)
+    for message in messages:
+         message_words = (re.sub(r"([^a-z0-9 '\-])", '', message)).split(" ")
+         for n in range(len(message_words)):
+              if n != len(message_words) - 1:
+                    try:
+                        bigram_dictionary[f"{message_words[n]} {message_words[n+1]}"]["seen"] += 1
+                    except(KeyError):
+                        try:
+                            bigram_dictionary[f"{message_words[n]} {message_words[n+1]}"] = {"seen": 1, "emotions": [dictionary[message_words[n]]["emotion"], dictionary[message_words[n+1]]["emotion"]]}
+                        except(KeyError):
+                             continue
+
+with open("bigramDictionary.json", 'w') as bigram_dictionary_file:
+     json.dump(bigram_dictionary, bigram_dictionary_file, indent=4)
+
+trigram_dictionary = {}
+
+for emotion in nEmotions:
+    messages = [message for message in message_dict2[emotion].keys()]
+    for response in message_dict2[emotion].values():
+        messages.extend(response)
+    for message in messages:
+         message_words = (re.sub(r"([^a-z0-9 '\-])", '', message)).split(" ")
+         for n in range(len(message_words)):
+              if n != 0 and n != len(message_words) - 1:
+                    try:
+                        trigram_dictionary[f"{message_words[n-1]} {message_words[n]} {message_words[n+1]}"]["seen"] += 1
+                    except(KeyError):
+                        try:
+                            trigram_dictionary[f"{message_words[n-1]} {message_words[n]} {message_words[n+1]}"] = {"seen": 1, "emotions": [dictionary[message_words[n-1]]["emotion"], dictionary[message_words[n]]["emotion"], dictionary[message_words[n+1]]["emotion"]]}
+                        except(KeyError):
+                             continue
+
+with open("trigramDictionary.json", 'w') as trigram_dictionary_file:
+     json.dump(trigram_dictionary, trigram_dictionary_file, indent=4)
